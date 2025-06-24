7294f79483c568df98b1bc8d1d6c47faa385e5d4
Bruno Arine <bruno.arine@runbox.com>
2025-06-24 12:20:17 -0300

    refactor: is_connected function to accept PID+VID~

---
+++ README.md
@@ -0,0 +1,7 @@
+# TEMPers
+
+A simple Rust utility to read TEMPer v1.4 USB thermometers.
+
+## Instructions
+
+1. Copy `70-temper.rules` into `/etc/udev/rules.d/`
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,60 @@
-// fn main() {
-//     for device in rusb::devices().unwrap().iter() {
-//         let device_desc = device.device_descriptor().unwrap();
+use hidapi::{HidApi, HidError};
+use std::process::exit;
 
-//         println!(
-//             "Bus {:03} Device {:03} ID {}:{}",
-//             device.bus_number(),
-//             device.address(),
-//             device_desc.vendor_id(),
-//             device_desc.product_id()
-//         );
-//     }
-// }
-
-use rusb::{DeviceList, GlobalContext};
-
+// TEMPer v1.4 (or any variant using the FM75 chip)
 const VENDOR_ID: u16 = 0x0c45;
 const PRODUCT_ID: u16 = 0x7401;
 
+const TIMEOUT_MS: i32 = 1000;
+
 /// Checks whether a specific USB device is present in the device list.
-///
-/// The function searches for a device matching the predefined vendor ID (`0x0c45`)
-/// and product ID (`0x7401`).
-///
-/// # Arguments
-/// * `devices` - A list of USB devices to search through
-///
-/// # Returns
-/// `true` if a matching device is found, `false` otherwise
-fn is_connected(devices: DeviceList<GlobalContext>) -> bool {
-    devices.iter().any(|d| -> bool {
-        let descriptor_result = d.device_descriptor();
-        match descriptor_result {
-            Ok(descriptor) => {
-                descriptor.vendor_id() == VENDOR_ID && descriptor.product_id() == PRODUCT_ID
-            }
-            Err(_) => false,
-        }
-    })
+fn is_connected(vendor_id: u16, product_id: u16, api: &HidApi) -> bool {
+    api.device_list()
+        .any(|d| d.product_id() == product_id && d.vendor_id() == vendor_id)
 }
 
-fn main() {
-    let devices = rusb::devices().unwrap();
-    println!("Device connected? {}", is_connected(devices));
+fn main() -> Result<(), HidError> {
+    let api = HidApi::new()?;
+    if !is_connected(VENDOR_ID, PRODUCT_ID, &api) {
+        eprintln!("Error: TEMPer device not connected");
+        exit(1);
+    }
+
+    let maybe_device_path = api
+        .device_list()
+        .find(|d| {
+            d.vendor_id() == VENDOR_ID && d.product_id() == PRODUCT_ID && d.interface_number() == 1
+        })
+        .map(|d| d.path());
+
+    let device_path = if let Some(path) = maybe_device_path {
+        path
+    } else {
+        eprintln!("Error: could not retrieve device path");
+        exit(1);
+    };
+
+    println!("Found TEMPer device at path: {:?}", device_path);
+    let device = api.open_path(&device_path)?;
+
+    let mut buffer: [u8; 8] = [0; 8];
+
+    // The first byte of data must contain the Report ID. For devices which only
+    // support a single report, this must be set to 0x0. The remaining bytes
+    // contain the report data. Therefore, calls to write() will always contain
+    // one more byte than the report contains.
+    let query: [u8; 9] = [0x00, 0x01, 0x80, 0x33, 0x01, 0x00, 0x00, 0x00, 0x00];
+
+    device.write(&query)?;
+    device.read_timeout(&mut buffer, TIMEOUT_MS)?;
+    println!("Buffer: {:?}", buffer);
+    println!("Temp: {:?}", convert_temper_buf(&buffer));
+
+    Ok(())
+}
+
+// Please see https://github.com/edorfaus/TEMPered/wiki/FM75
+fn convert_temper_buf(buf: &[u8; 8]) -> f32 {
+    let adj_temp = ((buf[2] as i32) << 8) + ((buf[3] as i32) & 0xFF);
+    (adj_temp as f32) * (125.0 / 32000.0)
 }
